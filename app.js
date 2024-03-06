0129163a8779fd161d0754bfad8d56e3823bb43a
gandharrdotexe <gandharbagde@gmail.com>
2024-03-07 00:38:07 +0530

    added the pop ups fixed: routing vulnerablities

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/homePage", (req, res) => {
   res.render("index");
 });
 
@@ -20,14 +20,23 @@ app.get("/get-started", (req, res) => {
   res.render("get-started");
 });
 
-app.get("/Text/:textId?", (req, res) => {
+app.get("/text/homePage", (req,res)=>{
+  res.redirect("/homepage");
+  res.render("index");
+});
+
+app.get("/text/:textId?", (req, res) => {
   var textId = req.params.textId;
   if (!textId) {
     var randomkey = generateRandomKey(5);
-    return res.redirect("/Text/" + randomkey);
+    return res.redirect("/text/" + randomkey);
   }
   res.render("text-share");
 });
 
 
+
+
 app.listen(3000, () => console.log("Running on port 3000"));
+
+
