4c3826ce26b28fd3ba19507603c23c9f26a223d1
Mithrajeeth18 <mithra86753@gmail.com>
2024-03-06 18:35:05 +0530

    0.1 changes_by_mj

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+
 app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/index',(req,res)=>{
-    res.render('index');
+app.get("/index", (req, res) => {
+  res.render("index");
 });
 
-app.get('/get-started',(req, res)=>{
-    res.render('get-started');
+app.get("/get-started", (req, res) => {
+  res.render("get-started");
 });
 
-app.get('/text-share',(req,res)=>{
-    res.render('text-share');
+app.get("/Text/:textId?", (req, res) => {
+  var textId = req.params.textId;
+  if (textId) {
+    console.log(textId);
+  }
+
+  res.render("text-share");
 });
 
-app.listen(3000, () => console.log("Running on port 3000"));
+app.listen(3000, () => console.log("Running on port 3000"));
