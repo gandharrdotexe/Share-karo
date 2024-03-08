c24ed3df62803382ab2ed330023532abc9e46c49
Mithrajeeth18 <mithra86753@gmail.com>
2024-03-08 23:54:48 +0530

    0.7 database text sharing

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
 const express = require("express");
 const { generateRandomKey, encrypt, decrypt } = require("./functions");
 const { MongoClient, GridFSBucket } = require("mongodb");
+
 const app = express();
 const helmet = require("helmet");
-const encyptKey = "Mithrajeeth18";
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 app.set("view engine", "ejs");
-var textId = "";
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(helmet());
 
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "your_secret_key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "https://cdn.jsdelivr.net",
+        "https://unpkg.com",
+      ],
+      connectSrc: ["'self'", "https://lottie.host"],
+      // Add other directives as needed
+    },
+  })
+);
+
+// variable diclaration
 const uri =
   "mongodb+srv://Mithra707:*Mithrajeeth*@cluster0.2gpt44h.mongodb.net";
+const encyptKey = "Mithrajeeth18";
+var textId = "";
+const data = {
+  content: "",
+  error: "",
+  Filestatus: "",
+  Filekey: "",
+};
 const client = new MongoClient(uri);
+let gfs;
 
+// connection chalu
 client
   .connect()
   .then(() => {
@@ -25,47 +63,61 @@ client
   })
   .catch((err) => console.error(err));
 
-  app.use(
-    helmet.contentSecurityPolicy({
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'", "https://cdn.jsdelivr.net", "https://unpkg.com"],
-        connectSrc: ["'self'", "https://lottie.host"]
-        // Add other directives as needed
-      },
-    })
-  );
-  
-
+// routes
 app.get("/", (req, res) => {
   res.render("index");
-});
-
-app.get("/homePage", (req, res) => {
-  //res.render("index");
-  res.redirect("/");
 });
 
 app.get("/get-started", (req, res) => {
   res.render("get-started");
 });
 
-app.get("/Text/homePage" || "/homePage" , (req, res) => {
+app.get(["/Text/homePage", "/homePage", "/File/homePage"], (req, res) => {
   res.redirect("/");
 });
 
 app.post("/Text/save", async (req, res) => {
   var encryptData = encrypt(req.body.content, encyptKey);
-  console.log(req.body.content);
+  //console.log(req.body.content);
+  await client
+    .db("Share-Note")
+    .collection("Data")
+    .updateOne(
+      { _id: textId },
+      { $set: { content: encryptData } },
+      { upsert: true }
+    );
+
+  return res.redirect("/Text/" + textId);
 });
 
-app.get("/Text/:textId?", (req, res) => {
+app.get("/Text/:textId?", async (req, res) => {
   textId = req.params.textId;
+
   if (!textId) {
     var randomkey = generateRandomKey(5);
     return res.redirect("/Text/" + randomkey);
   }
-  res.render("text-share");
+
+  try {
+    const containsData = await client
+      .db("Share-Note")
+      .collection("Data")
+      .findOne({ _id: textId });
+    const isLocked = await client
+      .db("Share-Note")
+      .collection("Lock")
+      .findOne({ _id: textId });
+
+    data["content"] = containsData
+      ? decrypt(containsData.content, encyptKey)
+      : "";
+
+    return res.render("text-share", data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.get("/File/:fileId?", (req, res) => {
